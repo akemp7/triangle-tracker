43ca8e16d7677eb2f940eafef5701da4bcc25050
Anita Kemp and Jason Huels <anita@>
2019-08-15 14:49:33 -0700

    refactor the code

--- js/scripts.js
+++ js/scripts.js
@@ -5,34 +5,25 @@ $(function() {
     var side3 = parseFloat($("input#side3").val());
 
     $(".triangle").hide();
-    //Math.max(side1, side2, side3);
-    if(!side1 || !side2 || !side3) {
-      $("p#output").text("You forgot to enter a value");
-    } else {
-      if(side1 < 1 || side2 < 1 || side3 < 1) {
-        $("p#output").text("Triangle sides must be greater than 0");
-      } else {
-        if(side1 + side2 <= side3/2 || side1 + side3 <= side2/2 || side2 + side3 <= side1/2) {
-          $("p#output").text("Oh no!");
-        } else {
-          if(side1 === side2 && side2 === side3) {
-            $("p#output").text("equalateral");
-            $("#equalateral").show();
-          } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-            $("p#output").text("isosceles");
-            $("#isosceles").show();
-          } else {
-            $("p#output").text("scalene");
-            $("#scalene").show();
-          }
 
+    if(side1 > 0 && side2 > 0 && side3 > 0) {
+      if(side1 + side2 <= side3/2 || side1 + side3 <= side2/2 || side2 + side3 <= side1/2) {
+        $("p#output").text("Oh no!");
+      } else {
+        if(side1 === side2 && side2 === side3) {
+          $("p#output").text("equalateral");
+          $("#equalateral").show();
+        } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+          $("p#output").text("isosceles");
+          $("#isosceles").show();
+        } else {
+          $("p#output").text("scalene");
+          $("#scalene").show();
         }
       }
+    } else {
+      $("p#output").text("Triangle sides must be greater than 0");
     }
-
-
-
-
 
     event.preventDefault();
   });
