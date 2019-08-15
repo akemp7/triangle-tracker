90c2b092919a7d7b59a6bb07e508983ec570f970
Anita Kemp and Jason Huels <jason@>
2019-08-15 16:09:18 -0700

    add more user friendly form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(function() {
   $("#submit").click(function(event) {
-    var side1 = parseFloat($("input#side1").val());
-    var side2 = parseFloat($("input#side2").val());
-    var side3 = parseFloat($("input#side3").val());
+    var side1 = parseFloat($("input#validationServer01").val());
+    var side2 = parseFloat($("input#validationServer02").val());
+    var side3 = parseFloat($("input#validationServer03").val());
 
     $(".triangle").hide();
+    $("#validationServer01").removeClass("is-invalid");
+    $("#validationServer02").removeClass("is-invalid");
+    $("#validationServer03").removeClass("is-invalid");
 
     if(side1 > 0 && side2 > 0 && side3 > 0) {
       if(side1 + side2 <= side3/2 || side1 + side3 <= side2/2 || side2 + side3 <= side1/2) {
@@ -23,6 +26,16 @@ $(function() {
       }
     } else {
       $("p#output").text("Triangle sides must be greater than 0");
+      if(!side1 || side1 < 1) {
+        $("#validationServer01").addClass("is-invalid");
+      }
+      if(!side2 || side2 < 1) {
+        $("#validationServer02").addClass("is-invalid");
+      }
+      if(!side3 || side3 < 1) {
+        $("#validationServer03").addClass("is-invalid");
+      }
+
     }
 
     event.preventDefault();
